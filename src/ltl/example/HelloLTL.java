22410e885ee4e6590d8c91222bc9d6c0e0fe50ad
Jason Bogle <kicker10bog@gmail.com>
2015-06-30 14:02:09 -0500

    minor additions
    added comments to first few LTL formulas and added a function that gets
    the predicates from the formula.

--- src/ltl/example/HelloLTL.java
+++ src/ltl/example/HelloLTL.java
@@ -1,23 +1,30 @@
 package ltl.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import gov.nasa.ltl.trans.Formula;
 import gov.nasa.ltl.trans.ParseErrorException;
 
 public class HelloLTL {
 	public static void main(String[] args) {
 		String[] formulas = {
-		                     "p",
-		                     "!p",
-		                     "p /\\ q",
-		                     "p \\/ q",
-		                     "p -> q",
-		                     "[] p",
-		                     "<> p",
-		                     "X p",
-		                     "p U q",
-		                     "p W q",
-		                     "p V q",
-		                     "( X p ) U ( X q )",
+		                     "p",	// p
+		                     "!p",	// NOT p
+		                     "p /\\ q", // p /\ q ... p AND q
+		                     "p \\/ q", // p \/ q ... p OR q
+		                     "program -> quick",	// program IMPLIES quick
+		                     "[] program",	// ALWAYS program
+		                     "<> program",	// 	EVENTUALLY program
+		                     "X program",	// NEXT program
+		                     "program U quiet",	// program UNTIL quiet
+		                     "p W q",	// p WEAK UNTIL q
+		                     "p V q",	// p RELEASE q
+		                     "p M q",	// p WEAK RELEASE q
+		                     /* You get the idea */
+		                     "( X p ) U ( X q )",	
 		                     "X ( p U q )",
 		                     "( p V q ) /\\ ( p V r )",
 		                     "p V ( q /\\ r )",
@@ -52,10 +59,28 @@ public class HelloLTL {
 			try {
 				System.out.println(f);
 				Formula.parse(f);
+				String[] predicates = getPredicates(f);
+				for (String p : predicates) {
+					System.out.println("->"+p);
+				}
+				System.out.println();
 			} catch (ParseErrorException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	public static String[] getPredicates(String formula) {
+		List<String> predicates = new ArrayList<String>();
+		Pattern pattern = Pattern.compile("([a-z][a-zA-Z0-9]*)");
+		Matcher matcher = pattern.matcher(formula);
+		while (matcher.find()) {
+			if (!matcher.group().equals("true") && !matcher.group().equals("false")) {
+				predicates.add(matcher.group());
+				//System.out.println(" ->"+matcher.group());
+			}
+		}
+		return (String[]) predicates.toArray(new String[predicates.size()]);
 	}
 }
